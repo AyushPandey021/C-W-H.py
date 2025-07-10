3e34366d556dfd08c0452ca892d89847b399100d
Ayushpaney24 <ayushpandey4480@gmail.com>
2025-07-10 22:02:01 +0530

    dictionary

--- CWH-DS/desnary-ds.py
+++ CWH-DS/desnary-ds.py
@@ -1,5 +1,13 @@
 # four ds is a dictionaries 
 # dictionaries store key-value pairs and allow fast looksups.
-marks={"ayush:34","kittu:24", "ritik:55"}
-print(marks.keys())
+# marks={"ayush:34","kittu:24", "ritik:55"}
+# print(marks.keys())
 # print(student.values())
+# this is a key value pairs  use the data value like map.
+my_dict = {
+    "name": "Ayush",
+    "age": 22,
+    "course": "Python"
+}
+
+print(my_dict.keys())
